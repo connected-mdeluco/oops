7a1c3656c5ed34a4e8295638e0e66130fbdb99a8
Matt DeLuco <mdeluco@connectedlab.com>
2019-08-14 15:44:31 -0400

    Adds IBDesignable extension to UIButton, adds Cancel/Confirm buttons to ScanFirst storyboard.

---
+++ Toybox/Toybox/Button.swift
@@ -0,0 +1,41 @@
+//
+//  Button.swift
+//  Toybox
+//
+//  Created by cl-dev on 2019-08-14.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
